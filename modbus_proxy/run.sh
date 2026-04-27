c89f9a12e6dc3d07acdab8e65e5a169e80f40d0c
lapinmaki <140404416+lapinmaki@users.noreply.github.com>
2026-04-27 18:57:40 +0300

    Add files via upload

--- modbus_proxy/run.sh
+++ modbus_proxy/run.sh
@@ -8,14 +8,6 @@ bashio::log.info "Starting Modbus Proxy..."
 bashio::log.info "Listening on 0.0.0.0:${LISTEN_PORT}"
 bashio::log.info "Forwarding to ${MODBUS_HOST}:${MODBUS_PORT}"
 
-cat > /tmp/modbus-proxy.yml << YMLEOF
-devices:
-  - modbus:
-      url: "${MODBUS_HOST}:${MODBUS_PORT}"
-      timeout: 10
-      connection_time: 1.0
-    listen:
-      bind: "0.0.0.0:${LISTEN_PORT}"
-YMLEOF
-
-exec modbus-proxy -c /tmp/modbus-proxy.yml
+exec modbus-proxy \
+  -b "tcp://0.0.0.0:${LISTEN_PORT}" \
+  --modbus "tcp://${MODBUS_HOST}:${MODBUS_PORT}"
